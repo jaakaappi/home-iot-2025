31b35bf3f289441dfdef8ca632d2aedc0cdefc2a
Esa Niemi <esa.niemi@protonmail.com>
2025-06-22 16:46:21 +0300

    Calibration for new balcony

--- backend/src/main/kotlin/com/homeiot/_5/DataConverter.kt
+++ backend/src/main/kotlin/com/homeiot/_5/DataConverter.kt
@@ -6,12 +6,19 @@ import java.math.RoundingMode
 
 @Component
 class DataConverter {
-    val RESISTIVE_HIGH = 2660.0f
-    val RESISTIVE_LOW = 2100.0f
-    val CAPACITIVE_1_HIGH = 2205.0f
-    val CAPACITIVE_1_LOW = 1265.0f
+    val RESISTIVE_HIGH = 1140.0f
+    val RESISTIVE_LOW = 350.0f
+//    val RESISTIVE_HIGH = 2660.0f
+//    val RESISTIVE_LOW = 2100.0f
+
+    //    val CAPACITIVE_1_HIGH = 2205.0f
+    val CAPACITIVE_1_HIGH = 1600.0f
+    val CAPACITIVE_1_LOW = 1100.0f
+
     val RESISTIVE_2_HIGH = 2700.0f
     val RESISTIVE_2_LOW = 2200.0f
+//    val RESISTIVE_2_HIGH = 1140.0f
+//    val RESISTIVE_2_LOW = 350.0f
 
     private fun convertReading(reading: Float, highLimit: Float, lowLimit: Float, invert: Boolean? = true): Float {
         val scaled = (reading - lowLimit) / (highLimit - lowLimit)
@@ -31,7 +38,7 @@ class DataConverter {
             data.brightness,
             convertReading(data.soilHumidity1, RESISTIVE_HIGH, RESISTIVE_LOW, invert = false).coerceIn(0.0f, 100.0f),
             convertReading(data.soilHumidity2, CAPACITIVE_1_HIGH, CAPACITIVE_1_LOW).coerceIn(0.0f, 100.0f),
-            convertReading(data.soilHumidity3, RESISTIVE_2_HIGH, RESISTIVE_2_LOW, invert = false).coerceIn(
+            convertReading(data.soilHumidity3, RESISTIVE_2_HIGH, RESISTIVE_2_LOW).coerceIn(
                 0.0f,
                 100.0f
             )
